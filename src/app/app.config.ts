0d430029f40f8d730a3f1b6d074de118febd12c4
Guilherme Maciel <sncglm@gmail.com>
2024-09-16 11:43:27 -0300

    FEAT: Adicionado e configurado NGRX DevTools Configurado o reducer dos jogos no app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { gamesReducer } from './state/game.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore(), provideEffects()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    provideStore({ games: gamesReducer }),
+    provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
