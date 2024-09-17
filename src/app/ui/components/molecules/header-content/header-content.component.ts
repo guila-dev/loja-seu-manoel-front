4aa2d84479d67dd9ee9ed9facadeba191c81a4a8
Guilherme Maciel <sncglm@gmail.com>
2024-09-16 23:42:18 -0300

    FIX: Adicionado posteriormente o header-content

---
+++ src/app/ui/components/molecules/header-content/header-content.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, signal } from '@angular/core';
+import { HeaderTitleComponent } from '../../atoms/header-title/header-title.component';
+import { IconeComponent } from '../../atoms/icone/icone.component';
+import { ButtonComponent } from '../../atoms/button/button.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header-content',
+  standalone: true,
+  imports: [HeaderTitleComponent, IconeComponent, ButtonComponent],
+  templateUrl: './header-content.component.html',
+  styleUrl: './header-content.component.css'
+})
+export class HeaderContentComponent {
+  router = inject(Router);
+
+  redirectToHome(){
+    this.router.navigate(['home'])
+  }
+  redirectToNewGame(){
+    this.router.navigate(['new-game'])
+  }
+}
